806bcc54bfe16571dcb030e78934e3f60c567573
Jason Gantsio <jasonondia@gmail.com>
2023-01-23 23:10:41 +0100

    Modif du fichier dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import os, sys, time
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 import requests
 import json
 import shap
@@ -12,9 +13,10 @@ from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.preprocessing import RobustScaler
 from sklearn.pipeline import Pipeline
 from streamlit_shap import st_shap
+
 import re
 
-st.title('Realiser un Dashboard')
+st.title('REALISER UN TABLEAU DE BORD')
 # Données sur les caracterisques d'un client
 #st.subheader('Echantillon des données ')
 df_client=pd.read_csv('df_projet')
@@ -30,10 +32,10 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score,accuracy_score
 
 # fastapi endpoint
-url =' http://127.0.0.1:8000/predict'
-endpoint = '/predict'
+#url =' http://127.0.0.1:8000/predict'
+#endpoint = '/predict'
 
-
+#url="https://test-projet7-2022.herokuapp.com/predict"
 
 feat = [f for f in df_client.columns if f not in ['TARGET','SK_ID_CURR','DAYS_EMPLOYED']]
 test1=df_client['SK_ID_CURR'].tolist()
@@ -41,14 +43,14 @@ number = st.sidebar.selectbox('Client ID',test1)
 number=int(number)
 if number in test1:
     st.write('The client number is ', number)
-    test=df_client['PAYMENT_RATE'].loc[df_client['SK_ID_CURR']==number].values
+    test=df_client['EXT_SOURCE_3'].loc[df_client['SK_ID_CURR']==number].values
     EXT_SOURCE_1=df_client['EXT_SOURCE_1'].loc[df_client['SK_ID_CURR']==number].values
     EXT_SOURCE_2=df_client['EXT_SOURCE_2'].loc[df_client['SK_ID_CURR']==number].values
     EXT_SOURCE_3=df_client['EXT_SOURCE_3'].loc[df_client['SK_ID_CURR']==number].values
-    AMT_GOODS_PRICE=df_client['AMT_GOODS_PRICE'].loc[df_client['SK_ID_CURR']==number].values
-    INSTAL_AMT_PAYMENT_MIN=df_client['INSTAL_AMT_PAYMENT_MIN'].loc[df_client['SK_ID_CURR']==number].values
-    PAYMENT_RATE=df_client['PAYMENT_RATE'].loc[df_client['SK_ID_CURR']==number].values
-    DAYS_EMPLOYED=df_client['DAYS_EMPLOYED'].loc[df_client['SK_ID_CURR']==number].values
+    DAYS_EMPLOYED_PERC=df_client['DAYS_EMPLOYED_PERC'].loc[df_client['SK_ID_CURR']==number].values
+    AMT_ANNUITY=df_client['AMT_ANNUITY'].loc[df_client['SK_ID_CURR']==number].values
+    PREV_CNT_PAYMENT_MEAN=df_client['PREV_CNT_PAYMENT_MEAN'].loc[df_client['SK_ID_CURR']==number].values
+    ACTIVE_DAYS_CREDIT_MAX=df_client['ACTIVE_DAYS_CREDIT_MAX'].loc[df_client['SK_ID_CURR']==number].values
     
 
     vector = np.vectorize(np.float64)
@@ -56,33 +58,70 @@ if number in test1:
     EXT_SOURCE_1=float(vector( EXT_SOURCE_1))
     EXT_SOURCE_2=float(vector( EXT_SOURCE_2))
     EXT_SOURCE_3=float(vector( EXT_SOURCE_3))
-    AMT_GOODS_PRICE=float(vector( AMT_GOODS_PRICE))
-    INSTAL_AMT_PAYMENT_MIN=float(vector( INSTAL_AMT_PAYMENT_MIN))
-    PAYMENT_RATE=float(vector( PAYMENT_RATE))
-    DAYS_EMPLOYED=float(vector( DAYS_EMPLOYED))
+    DAYS_EMPLOYED_PERC=float(vector( DAYS_EMPLOYED_PERC))
+    AMT_ANNUITY=float(vector( AMT_ANNUITY))
+    PREV_CNT_PAYMENT_MEAN=float(vector( PREV_CNT_PAYMENT_MEAN))
+    ACTIVE_DAYS_CREDIT_MAX=float(vector( ACTIVE_DAYS_CREDIT_MAX))
     
 
-    valeurs={"EXT_SOURCE_1": EXT_SOURCE_1,"EXT_SOURCE_2": EXT_SOURCE_2,"EXT_SOURCE_3": EXT_SOURCE_3,"AMT_GOODS_PRICE":AMT_GOODS_PRICE,"INSTAL_AMT_PAYMENT_MIN":INSTAL_AMT_PAYMENT_MIN,"PAYMENT_RATE": PAYMENT_RATE,"DAYS_EMPLOYED":DAYS_EMPLOYED}
+    valeurs={"EXT_SOURCE_1": EXT_SOURCE_1,"EXT_SOURCE_2": EXT_SOURCE_2,"EXT_SOURCE_3": EXT_SOURCE_3,"DAYS_EMPLOYED_PERC":DAYS_EMPLOYED_PERC,"AMT_ANNUITY":AMT_ANNUITY,"PREV_CNT_PAYMENT_MEAN": PREV_CNT_PAYMENT_MEAN,"ACTIVE_DAYS_CREDIT_MAX":ACTIVE_DAYS_CREDIT_MAX}
     val=json.dumps(valeurs)
+    essai={0:{"EXT_SOURCE_1": EXT_SOURCE_1,"EXT_SOURCE_2": EXT_SOURCE_2,"EXT_SOURCE_3": EXT_SOURCE_3,"DAYS_EMPLOYED_PERC":DAYS_EMPLOYED_PERC,"AMT_ANNUITY":AMT_ANNUITY,"PREV_CNT_PAYMENT_MEAN": PREV_CNT_PAYMENT_MEAN,"ACTIVE_DAYS_CREDIT_MAX":ACTIVE_DAYS_CREDIT_MAX}}
+    val_df=pd.DataFrame(essai)
+    val_df=val_df.transpose()
+     
+    features=["EXT_SOURCE_2","EXT_SOURCE_3","EXT_SOURCE_1","DAYS_EMPLOYED_PERC","AMT_ANNUITY","PREV_CNT_PAYMENT_MEAN","ACTIVE_DAYS_CREDIT_MAX"] 
     
-
-    st.write(valeurs)
+    st.subheader('INFORMATIONS DU CLIENT')
+    st.write(val_df)
+    st.subheader('INFORMATIONS DESCRIPTIVES DES CLIENTS')
+    st.write(df_client[features].describe())
     if st.button("Predict"):
-        response = requests.post(url="https://test-projet7-2022.herokuapp.com/predict",data=val)
+        response = requests.post(url =' https://test-projet7-2022.herokuapp.com/predict',data=val)
         #prediction =response.text
-        st.subheader(f"The prediction from model and probability : {response.text}")
+        #st.subheader(f"The prediction from model and probability : {response.text}")
+        REP=response.json()
+        KEY_1=REP['prediction']
+        KEY_2=REP['probability']
+        categories=['OCTROI','REFUS']
+        
+        if KEY_1==0:
+            st.subheader('DEMANDE DE CREDIT ACCEPTE')
+            proba=[KEY_2,1-KEY_2]
+            fig, ax = plt.subplots()
+            #plt.figure(figsize=(4,4))
+            plt.style.use('dark_background')
+            colors=['indigo','navy']
+            explode=[0.09,0]
+            ax.pie(proba,labels=categories,colors=colors,explode=explode,autopct='%1.2f%%')
+            st.pyplot(fig)
+        if KEY_1==1:
+            st.subheader('DEMANDE DE CREDIT REFUSE')
+            proba=[1-KEY_2,KEY_2]
+            fig1, ax1 = plt.subplots()
+            plt.style.use('dark_background')
+            colors=['indigo','navy']
+            explode=[0,0.09]
+            ax1.pie(proba,labels=categories,colors=colors,explode=explode,autopct='%1.2f%%')
+            st.pyplot(fig1)
+            
+
     
-    if st.button('Interprétabilité des résultats'):
-        #shap.initjs()
-        model = load('modele_projet7')
-        classifier=model['HGBClassifier']
-        user=pd.DataFrame([valeurs])
-        #st.write(user)
-        x_transfo= model['RobustScaler'].fit_transform(user)
-        explainer = shap.TreeExplainer(classifier)
-        shap_values = explainer.shap_values(x_transfo)
+    if st.button('IMPORTANCE DES VARIABLES'):
+        x=[EXT_SOURCE_1,EXT_SOURCE_2,EXT_SOURCE_3,DAYS_EMPLOYED_PERC,AMT_ANNUITY,PREV_CNT_PAYMENT_MEAN,ACTIVE_DAYS_CREDIT_MAX]
+        url=f'https://test-projet7-2022.herokuapp.com/INTERPRETABILITE?f1={x[0]}&f2={x[1]}&f3={x[2]}&f4={x[3]}&f5={x[4]}&f6={x[5]}&f7={x[6]}'
+        resultat = requests.get(url )
         
-        features=["EXT_SOURCE_2","EXT_SOURCE_3","EXT_SOURCE_1","AMT_GOODS_PRICE","INSTAL_AMT_PAYMENT_MIN","PAYMENT_RATE","DAYS_EMPLOYED"]
+        #st.subheader(resultat.json())
+        #val=resultat.text
+        result=resultat.json()
+        clé_1=result['shap_values']
+        clé_2=result['x_transfo']
+        shap_values=np.array(clé_1)
+        x_transfo=np.array(clé_2)
+        plt.style.use('Solarize_Light2')
+        features=["EXT_SOURCE_2","EXT_SOURCE_3","EXT_SOURCE_1","DAYS_EMPLOYED_PERC","AMT_ANNUITY","PREV_CNT_PAYMENT_MEAN","ACTIVE_DAYS_CREDIT_MAX"]
         st_shap(shap.summary_plot(shap_values,x_transfo,plot_type='bar',feature_names=features,max_display=len(features)))
-        
+        #st.write(shap_values)
+        #st_shap(shap.plots.waterfall(shap_values[0]))
         
